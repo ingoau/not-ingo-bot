68d8715be3026c3423f5470f8fed9f87a22e5a0e
Ingo Wolf <me@ingowolf.au>
2026-02-05 11:31:57 +1100

    return useful data

--- src/convex/backgroundChecks.ts
+++ src/convex/backgroundChecks.ts
@@ -4,10 +4,16 @@ import { v } from "convex/values";
 import { internalAction } from "./_generated/server";
 import { WebClient } from "@slack/web-api";
 
+const returnType = {
+  pass: v.boolean(),
+  message: v.optional(v.string()),
+};
+
 export const idv = internalAction({
   args: {
     slackId: v.string(),
   },
+  returns: returnType,
   handler: async (ctx, { slackId }) => {
     const idvRequest = await fetch(
       "https://auth.hackclub.com/api/external/check?slack_id=" + slackId,
@@ -22,6 +28,16 @@ export const idv = internalAction({
         | "rejected" // Verification was rejected
         | "not_found"; // No user found with the provided identifier
     };
-    return idvResponse;
+
+    if (idvResponse.result === "verified_eligible") {
+      return {
+        pass: true,
+      };
+    } else {
+      return {
+        pass: false,
+        message: "IDV status: " + idvResponse.result,
+      };
+    }
   },
 });
