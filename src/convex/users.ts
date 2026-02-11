7cdd23637b9cc3a6c68ffd21cb6cccf1f97a2f9e
Ingo Wolf <me@ingowolf.au>
2026-02-12 10:58:59 +1100

    add syncuserinfo action

--- src/convex/users.ts
+++ src/convex/users.ts
@@ -1,8 +1,72 @@
-import { internalQuery } from "./_generated/server";
+import { v } from "convex/values";
+import { internal } from "./_generated/api";
+import { Id } from "./_generated/dataModel";
+import {
+  internalAction,
+  internalMutation,
+  internalQuery,
+} from "./_generated/server";
 
 export const list = internalQuery({
   handler: async (ctx) => {
     const users = await ctx.db.query("users").collect();
     return users;
+  },
+});
+
+export const syncUserInfo = internalAction({
+  handler: async (ctx) => {
+    const users = await ctx.runQuery(internal.users.list);
+    const usersWithInfo: {
+      id: Id<"users">;
+      slackDisplayName: string | undefined;
+      slackName: string | undefined;
+      isBot: boolean;
+    }[] = [];
+    await Promise.all(
+      users.map(async (user) => {
+        try {
+          const userInfo = await ctx.runAction(internal.slack.getProfile, {
+            slackId: user.slackId,
+          });
+          usersWithInfo.push({
+            id: user._id,
+            slackDisplayName: userInfo.profile?.display_name,
+            slackName: userInfo.profile?.real_name,
+            isBot: (userInfo.profile && "bot_id" in userInfo.profile) || false,
+          });
+        } catch {
+          console.log("e");
+        }
+      }),
+    );
+
+    await ctx.runMutation(internal.users.saveUserInfo, {
+      users: usersWithInfo,
+    });
+  },
+});
+
+export const saveUserInfo = internalMutation({
+  args: {
+    users: v.array(
+      v.object({
+        id: v.id("users"),
+        slackDisplayName: v.optional(v.string()),
+        slackName: v.optional(v.string()),
+        isBot: v.boolean(),
+      }),
+    ),
+  },
+  handler: async (ctx, { users }) => {
+    await Promise.all(
+      users.map(async (user) => {
+        await ctx.db.patch(user.id, {
+          slackDisplayName: user.slackDisplayName,
+          slackName: user.slackName,
+          isBot: user.isBot,
+        });
+      }),
+    );
   },
 });
